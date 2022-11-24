a85601ecd2cf008f4e3ae235d17892afd6958381
Osoianu Dragos-Ioan <dragos.osoianu@gmail.com>
2022-11-24 18:03:35 +0100

    works
    works :)

---
+++ DNP-Assigment-main/Shared/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Shared.Models;
+
+public class User
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public string Name { get; set; }
+    public string Role { get; set; }
+    public int Age { get; set; }
+    [JsonIgnore]
+    public ICollection<Post>? Posts { get; set; }
+}
